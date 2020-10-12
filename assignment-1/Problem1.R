30d52c017afaaec573f6c0fbc8370cd4c67aaba8
Boyko Amarov <boyko.amarov@gmail.com>
2020-10-12 13:59:28 +0300

    Create Problem1.R

---
+++ assignment-1/Problem1.R
@@ -0,0 +1,11 @@
+# Assignment 1, Problem 1
+
+# a) Create character vector with the strings '2017-11-09', '2017-08-11'
+
+# b) Coerce the string vector to class Date
+
+# c) Numeric representation of dates
+
+# d) 
+
+# e)
